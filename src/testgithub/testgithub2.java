a654d0cb27689f7f66a287fa9f4b199ffcb62b06
Ayush <Ayush@DESKTOP-39RMEGT>
2025-06-22 12:20:48 +0530

    Second Commit

---
+++ src/testgithub/testgithub2.java
@@ -0,0 +1,11 @@
+package testgithub;
+
+public class testgithub2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Github");
+	}
+
+}
